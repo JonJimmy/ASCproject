6cec4a3058747857342e4dbfceecc4a2260bf166
Joana James <jonjimmy@Joanas-MacBook-Air.local>
2022-05-22 13:10:07 +0100

    First commit

--- audioprocessors.py
+++ audioprocessors.py
@@ -54,7 +54,7 @@ def processor_d18(file_path):
 
     spectrograms = np.asarray(spectrograms, dtype=np.float32)
 
-    return spectrograms
+    return spectrograms,s1
 
 
 def processor_d18_stereo(file_path):
